272dbeefa8bc884ac67483f48ae7cc46bfd541fd
Priyanshi Nigam <priyaaanigam.pn@gmail.com>
2020-09-24 19:02:36 -0700

    LinkedList Codes

---
+++ LINKEDLIST/SegregateLinkedList.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+       ListNode oddN = head;
+        ListNode evenN = head.next;
+        ListNode odd = oddN;
+        ListNode even = evenN;
+        
+        
+        
+        while(odd.next!=null && even.next!=null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenN;
+        return head;
+    }
+}
